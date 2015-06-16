9656b6f9ce3eecfb14d275fec601d4f53d1c3de2
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-06-15 22:47:00 -0400

    Update angular-drupal config for app.

--- app/app.js
+++ app/app.js
@@ -2,16 +2,16 @@
 
 angular.module('angularDrupalApp', ['angular-drupal']).
   run(['drupal', function(drupal) {
-        drupal.token().then(function(token) {
-            console.log(token);
-        });
+
+        // Use drupal here...
+
   }]);
 
 // Angular Drupal Configuration Settings
 angular.module('angular-drupal').config(function($provide) {
-    
+
     $provide.value('drupalSettings', {
-        site_path: 'http://localhost/drupal-7',
+        sitePath: 'http://localhost/drupal-7',
         endpoint: 'drupalgap'
     });
 
