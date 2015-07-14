9fcaa359c9be8f6f73229af07752ad44223a4430
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2015-07-14 10:42:34 -0400

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,17 +3,17 @@
 angular.module('angularDrupalApp', ['angular-drupal']).
   run(['drupal', function(drupal) {
 
-        // Use drupal here...
+    // Use drupal here...
 
   }]);
 
 // Angular Drupal Configuration Settings
 angular.module('angular-drupal').config(function($provide) {
 
-    $provide.value('drupalSettings', {
-        sitePath: 'http://localhost/drupal-7',
-        endpoint: 'drupalgap'
-    });
+  $provide.value('drupalSettings', {
+    sitePath: 'http://localhost/drupal-7',
+    endpoint: 'drupalgap'
+  });
 
 });
 
