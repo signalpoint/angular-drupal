16d360065c9d7874cd12fd9595183ac222969e62
KentR <info@kentrichards.net>
2016-03-02 07:04:07 -0800

    Convert to use jDrupal wrapped service.

--- app/app.js
+++ app/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
 angular.module('angularDrupalApp', ['angular-drupal']).
-  run(['drupal', function(drupal) {
+  run(['$drupal', function($drupal) {
 
     // Use drupal here...
-
+    $drupal.nodeLoad(1).then(function(node) {
+      console.log(node.getTitle());
+    });
   }]);
 
 // Angular Drupal Configuration Settings
 angular.module('angular-drupal').config(function($provide) {
 
   $provide.value('drupalSettings', {
-    sitePath: 'http://localhost/drupal-7',
-    endpoint: 'drupalgap'
+    sitePath: 'http://example.com',
   });
 
 });
-
