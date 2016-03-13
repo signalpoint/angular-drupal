3526712491d23864a7ebad7a747b23ed0a1a4ce6
Kent Richards <info@kentrichards.net>
2016-03-13 08:05:51 -0700

    Angularize some jDrupal promise functions with .

--- src/angular-drupal.js
+++ src/angular-drupal.js
@@ -9,8 +9,25 @@ angular.module('angular-drupal', [])
  * @param object drupalSettings
  *   Various settings.  Change these in app.js.
  */
-.factory('$drupal', function(drupalSettings) {
+.factory('$drupal', ['drupalSettings', '$q', function(drupalSettings, $q) {
   // jDrupal is initialized globally in jdrupal.js
   jDrupal.config('sitePath', drupalSettings.sitePath);
+  
+  // Swap promise functions with Angular versions
+  jDrupal.Entity.prototype._load = jDrupal.Entity.prototype.load;
+  jDrupal.Entity.prototype.load = function() {
+    return $q.when(this._load());
+  };
+  
+  jDrupal.Views.prototype._getView = jDrupal.Views.prototype.getView;
+  jDrupal.Views.prototype.getView = function() {
+    return $q.when(this._getView());
+  }
+  
+  jDrupal._viewsLoad = jDrupal.viewsLoad;
+  jDrupal.viewsLoad = function(path) {
+    return $q.when(this._viewsLoad(path));
+  }
+  
   return jDrupal;
-});
+}]);
