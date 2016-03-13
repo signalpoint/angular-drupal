214bf465df27b617e375044689bac217da827255
Kent Richards <info@kentrichards.net>
2016-03-13 08:13:30 -0700

    Revert service name to 'drupal'. (cherry picked from commit 0f51753b7e950adcc94bd4b899f228e7050795c9)

--- src/angular-drupal.js
+++ src/angular-drupal.js
@@ -4,12 +4,12 @@
 angular.module('angular-drupal', [])
 
 /**
- * The $drupal service for the angular-drupal module.
+ * The drupal service for the angular-drupal module.
  * 
  * @param object drupalSettings
  *   Various settings.  Change these in app.js.
  */
-.factory('$drupal', function(drupalSettings) {
+.factory('drupal', ['drupalSettings', '$q', function(drupalSettings, $q) {
   // jDrupal is initialized globally in jdrupal.js
   jDrupal.config('sitePath', drupalSettings.sitePath);
   return jDrupal;
